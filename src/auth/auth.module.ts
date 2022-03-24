ce56fa8bf5e1d5b316dc34cdaa66c0d380a2c142
Mpcsj <marcosjunioribm@gmail.com>
2022-03-23 23:32:25 -0300

    - :recycle: Update Auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
@@ -26,6 +27,11 @@ import { AuthService } from './auth.service';
         })
     ],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, JwtStrategy],
+    exports: [
+        AuthService,
+        JwtStrategy,
+        PassportModule
+    ],
 })
 export class AuthModule { }
